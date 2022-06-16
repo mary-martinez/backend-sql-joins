6369c8a6e1b4e929faf6bc9478182148e5b7fe24
Mary Martinez <mary.diana.martinez@gmail.com>
2022-06-15 18:17:37 -0700

    All exercises are complete, and all tests are passing!!

--- exercise_10/exercise-10.sql
+++ exercise_10/exercise-10.sql
@@ -1,2 +1,10 @@
 -- find the category name of all inventory items that have never been rented
 -- Similar to exercise 9 with an additonal join
+
+SELECT category.name
+FROM inventory
+LEFT JOIN rental ON inventory.inventory_id = rental.inventory_id
+-- LEFT JOIN film ON inventory.film_id = film.film_id
+LEFT JOIN film_category ON inventory.film_id = film_category.film_id
+LEFT JOIN category ON film_category.category_id = category.category_id
+WHERE rental.customer_id IS NULL
